c40155bb1e6b736524335c6b7d4aa19f189f8cd3
Seregfaroth <bl@tokni.com>
2015-10-01 10:08:22 +0100

    Starting fresh

---
+++ Source/SeaFoodSim48/SeaFoodSim48.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SeaFoodSim48.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SeaFoodSim48, "SeaFoodSim48" );
---
+++ Source/SeaFoodSim48/SeaFoodSimPlayerState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerState.h"
+#include "SeaFoodSimPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEAFOODSIM48_API ASeaFoodSimPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
